0c8405f7e01137fc1eeee190bd1ddc1f5afaadde
pipranja <pipranja@cisco.com>
2021-12-25 13:05:20 +0530

    initial commit

---
+++ src/main/java/com/assessment/codepipeline/api/Home.java
@@ -0,0 +1,14 @@
+package com.assessment.codepipeline.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
+
+}
